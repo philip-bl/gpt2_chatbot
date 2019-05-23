2a69006610ba278b2f006fbf368f0018066a883a
Philip B <me@crabman.me>
2019-05-23 17:38:12 +0300

    Add function to calc loss

--- gpt2/test_things.py
+++ gpt2/test_things.py
@@ -1,6 +1,8 @@
 from typing import *
 
 import torch
+import torch.nn as nn
+import torch.nn.functional as tnnf
 
 from pytorch_pretrained_bert import GPT2Tokenizer, GPT2LMHeadModel
 
@@ -14,6 +16,28 @@ def encode_many_texts(tokenizer: GPT2Tokenizer, texts: Iterable[str]) \
         text + [-1] * (max_len-len(text)) for text in encoded_texts
     ]
     return torch.tensor(padded_encoded_texts)
+
+
+def mask_for_forward(batch: torch.Tensor) -> torch.Tensor:
+    return torch.masked_fill(batch, batch == -1, 6666)
+
+def calculate_lm_loss(lm_logits: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
+    """Target may contain -1 in some entries, they will not contribute to loss.
+    lm_logits is the 0th element returned by model. target is batch before being
+    transformed by mask_for_forward."""
+    VOCAB_SIZE = 50257
+    batch_size, seq_len = target.shape
+    assert lm_logits.shape == (batch_size, seq_len, VOCAB_SIZE)
+    # Shift so that tokens < n predict n
+    shift_logits = lm_logits[:, :-1]
+    shift_labels = target[:, 1:]
+
+    # Flatten the tokens
+    loss = tnnf.cross_entropy(
+        shift_logits.reshape(-1, VOCAB_SIZE),
+        shift_labels.reshape(-1), ignore_index=-1
+    )
+    return loss
 
 
 tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
