0ef07f8c1a9455cef5058bfb4950ae0cd652aefa
matvich <matvineko.ivan.a@outlook.com>
2019-05-25 04:07:59 +0300

    Added simple wrapper, which works pretty strange

---
+++ gpt2_chatbot/wrapper.py
@@ -0,0 +1,125 @@
+from model_sampler import *
+
+import numpy as np
+import torch
+import torch.nn.functional as F
+from tqdm import trange
+
+from pytorch_pretrained_bert import GPT2LMHeadModel, GPT2Tokenizer
+
+def wrap_message_list(m_list, insert_intro=True, wrap_type='name', check_end_punct=True):
+    '''
+    Parameters:
+    ----------
+    m_list : list
+        list of messages in chatbot log 
+    insert_intro : bool, optional
+        whether should it insert the intro about the conversation
+    wrap_type : string, optional
+        type of conditioning to use ('name', 'name-in-par', 'dash', 'number') 
+    check_end_punct : bool, optional
+        whether should it check the last symbol of message to have the period etc
+    '''
+    output = ""
+    types = {'name': ('Alice: ', 'Bob: '),
+            'name-in-par': ('[Alice]: ', '[Bob]: '),
+            'dash': ('-', '-'),
+            'number': ('1: ', '2: ')}
+    valid_ending = ['.', '!', '?', '\'']
+    
+    assert wrap_type in types, "Unknown wrapping"
+    
+    if(insert_intro):
+        output += "This is the conversation between 2 people.\n"
+        
+    for i, msg in enumerate(m_list):
+        output += '\n'        
+        output += types[wrap_type][i%2]
+        output += msg
+        if((check_end_punct) and (msg[-1] not in valid_ending)):
+            output += '.'
+            
+    output += '\n'
+    output += types[wrap_type][(i+1)%2]
+    return output
+
+def init_model(seed=0, model_name_or_path='gpt2'):
+    '''
+    Parameters:
+    ----------
+    seed : int
+        seed number for different ramdomizers
+    model_name_or_path : string, optional
+        either model name for existing model or path for trained model (not realised yet)
+    '''
+    np.random.seed(seed)
+    torch.random.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    enc = GPT2Tokenizer.from_pretrained(model_name_or_path)
+    model = GPT2LMHeadModel.from_pretrained(model_name_or_path)
+    # model.load_state_dict(torch.load( ... )) uncommet it to make it work....
+    model.to(device)
+    model.eval()
+    return model, enc, device
+
+def model_forward(input_text, *model_params, length=-1, top_k=0, temperature=1.0):
+    '''
+    Parameters:
+    ----------
+    input_text : string
+        input text for sampling
+    *model_params : tuple
+        (model, enc, device) output of 'init_model' function
+    length : int, optional
+        length of generated sample I guess (!!not sure!!)
+    top_k : int, optional
+        to generate k most probable samples (!!not sure!!)
+    temperature: float, optional
+        parameter of sampling algorithm
+    '''
+
+    model, enc, device = model_params
+    if length == -1:
+        length = model.config.n_ctx // 2
+    elif length > model.config.n_ctx:
+        raise ValueError("Can't get samples longer than window size: %s" % model.config.n_ctx)
+        
+    context_tokens = []
+    context_tokens = enc.encode(input_text)
+
+    out = sample_sequence(
+        model=model, length=length,
+        context=context_tokens,
+        start_token=None,
+        batch_size=1,
+        temperature=temperature, top_k=top_k, device=device)
+
+    out = out[:, len(context_tokens):].tolist()
+    output_text = enc.decode(out[0])
+    return output_text
+
+def produce_answer(user_input, prev_msgs, *model_params, **wrap_params):
+    '''
+    Parameters:
+    ----------
+    user_input : string
+        user's message
+    prev_msgs : list
+        list of previous messages in conversation
+    *model_params : tuple
+        (model, enc, device) output of 'init_model' function
+    **wrap_parameters : dict
+        parametrs for 'wrap_message_list' function like `wrap_type`    
+    '''
+    prev_msgs.append(user_input)
+    input_text = wrap_message_list(prev_msgs, **wrap_params)
+    
+    sampled_answer = model_forward(input_text, *model_params)
+    print("All sampled:\n", sampled_answer, "\n\n")
+    answer = sampled_answer.split('\n')[0]
+    prev_msgs.append(answer)
+    return answer
+
